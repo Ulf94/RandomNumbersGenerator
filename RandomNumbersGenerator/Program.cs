b6de2841f8f617f858688819e0fda8540efbfc4c
Michal Szura <michal.szura@bytespack.com>
2023-03-08 12:56:47 +0100

    Add project files.

---
+++ RandomNumbersGenerator/Program.cs
@@ -0,0 +1,30 @@
+﻿var dictionary = new Dictionary<int, int>();
+
+foreach (var number in GetNumbers(1, 100_000_000))
+{
+    if (dictionary.ContainsKey(number) is false)
+        dictionary.Add(number, 1);
+    else
+    {
+        dictionary[number]++;
+    }
+}
+
+var maxValue = dictionary.Values.Max();
+
+var topOccurrence = dictionary.Where(number => number.Value == maxValue);
+
+foreach (var keyValuePair in topOccurrence)
+{
+    Console.WriteLine($"{keyValuePair.Key} : {keyValuePair.Value}");
+}
+
+IEnumerable<int> GetNumbers(int minValue, int maxValue)
+{
+    var random = new Random();
+
+    for (var i = 0; i <= maxValue; i++)
+    {
+        yield return random.Next(minValue, maxValue);
+    }
+}
